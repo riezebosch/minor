08ad83242c96fec665ad999a47f59bb59ab27f94
Manuel Riezebosch <mriezebosch@gmail.com>
2016-09-12 10:16:33 +0200

    configuratie staat goed, kan de eerste unit test nu uitvoeren

--- src/TddDemo/Class1.cs
+++ src/TddDemo/Class1.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace TddDemo
 {
     public class Class1
     {
-        public Class1()
+        [Fact]
+        public void MijnEersteTestMethode()
         {
+
         }
     }
 }
