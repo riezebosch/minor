f33657a25221b48902176468026da8da8e62deec
Manuel Riezebosch <mriezebosch@gmail.com>
2016-09-12 11:07:52 +0200

    beetje werkend, klas mag het verder doen

--- src/TddDemo/Class1.cs
+++ src/TddDemo/Class1.cs
@@ -11,7 +11,71 @@ namespace TddDemo
         [Fact]
         public void MijnEersteTestMethode()
         {
+            // Arrange
+            var geboorteDatum = new DateTime(1992, 10, 26);
+            var huidig = new DateTime(2016, 09, 12);
 
+            // Act
+            var leeftijd = BerekenLeeftijd(geboorteDatum, huidig);
+
+            // Assert
+            Assert.Equal(23, leeftijd);
+        }
+
+        [Fact]
+        public void EenAndereDatum()
+        {
+            // Arrange
+            var geboorteDatum = new DateTime(1991, 12, 31);
+            var huidig = new DateTime(2016, 09, 12);
+
+            // Act
+            var leeftijd = BerekenLeeftijd(geboorteDatum, huidig);
+
+            // Assert
+            Assert.Equal(24, leeftijd);
+        }
+
+        [Fact]
+        public void GeboorteDatumInDeToekomstGeeftFoutmelding()
+        {
+            // Arrange
+            var geboorteDatum = new DateTime(2017, 12, 31);
+            var huidig = new DateTime(2016, 09, 12);
+
+            // Act + Assert
+            Assert.Throws(typeof(ArgumentException) , () => BerekenLeeftijd(geboorteDatum, huidig));
+        }
+
+        [Fact]
+        public void WatAlsIemandNogNietJarigIsGeweest()
+        {
+            // Arrange
+            var geboorteDatum = new DateTime(2016, 09, 11);
+            var huidig = new DateTime(2016, 09, 12);
+
+            // Act
+            var leeftijd = BerekenLeeftijd(geboorteDatum, huidig);
+
+            // Assert
+            Assert.Equal(0, leeftijd);
+        }
+
+        private int BerekenLeeftijd(DateTime geboorteDatum, DateTime huidig)
+        {
+            if (geboorteDatum > huidig)
+            {
+                throw new ArgumentException($"Geboortedatum {geboorteDatum} moet kleiner zijn dan {huidig}.", 
+                    nameof(geboorteDatum));
+            }
+
+            var result = huidig.Year - geboorteDatum.Year;
+            if (geboorteDatum.Month != huidig.Month || geboorteDatum.Day >= huidig.Day)
+            {
+                result--;
+            }
+
+            return result;
         }
     }
 }
