35e82b4d33e384b901a77c1c647324d0cf6b5f4a
Manuel Riezebosch <mriezebosch@gmail.com>
2016-09-14 13:59:21 +0200

    list with single item add and get

--- src/TddDemo/LinkedListTest.cs
+++ src/TddDemo/LinkedListTest.cs
@@ -21,13 +21,45 @@ namespace TddDemo
             Assert.Equal(1, list.Count);
         }
 
+        [Fact]
+        public void GivenItemAddedItemWhenGetThenItemReturned()
+        {
+            // Arrange
+            var list = new LinkedList();
+            list.Add(3);
+
+            // Act
+            var item = list.Get(0);
+
+            // Assert
+            Assert.Equal(3, item);
+        }
+
         class LinkedList
         {
+            private Node first;
+
             public int Count { get; private set; }
 
             public void Add(int item)
             {
+                first = new Node(item);
                 Count++;
+            }
+
+            public object Get(int index)
+            {
+                return first.Item;
+            }
+
+            private class Node
+            {
+                public Node(int item)
+                {
+                    this.Item = item;
+                }
+
+                public object Item { get; internal set; }
             }
         }
     }
