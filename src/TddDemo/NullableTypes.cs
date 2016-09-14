3fb5992f5cc8746d47562ef1037c11601cfd7277
Manuel Riezebosch <mriezebosch@gmail.com>
2016-09-14 11:06:56 +0200

    ref, out en nullable casts

---
+++ src/TddDemo/NullableTypes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TddDemo
+{
+    public class NullableTypes
+    {
+        [Fact]
+        public void CastVanNullableTypes()
+        {
+            int x1 = 1;
+            int? x2 = null;
+
+            int x3 = x1;
+            int x4 = (int)x1;
+            // --> dit mag dus niet: int x5 = x2;
+            // --> dit geeft runtime een fout: int x6 = (int)x2;
+            
+
+            int? x7 = x1;
+            int? x8 = (int?)x1;
+            int? x9 = x2;
+
+            int x10 = x2 ?? 0;
+            Assert.Equal(0, x10);
+        }
+    }
+}
---
+++ src/TddDemo/RefAndOutDemo.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TddDemo
+{
+    public class RefAndOutDemo
+    {
+        [Fact]
+        public void PassByRef()
+        {
+            int x = 3;
+
+            Update(x);
+            Assert.Equal(3, x);
+
+            Update(ref x);
+            Assert.Equal(5, x);
+
+            var pien = new Hond { Aaibaarheid = 9 };
+            Update(pien);
+            Assert.Equal(9, pien.Aaibaarheid);
+
+            Update(ref pien);
+            Assert.Equal(1000, pien.Aaibaarheid);
+        }
+
+        private void Update(Hond hond)
+        {
+            hond = new Hond { Aaibaarheid = 1000 };
+            hond.Aaibaarheid++;
+        }
+
+        private void Update(ref Hond hond)
+        {
+            hond = new Hond { Aaibaarheid = 1000 };
+        }
+
+        private void Update(int x)
+        {
+            x = 5;
+        }
+
+        private void Update(ref int x)
+        {
+            x = 5;
+        }
+
+        class Hond
+        {
+            public int Aaibaarheid { get; set; }
+        }
+
+        [Fact]
+        public void WatIsHetVerschilTussenOutEnRef()
+        {
+            int x = 3;
+            int y;
+
+            Update(ref x);
+            Assert.Equal(5, x);
+
+            UpdateWithOut(out y);
+            Assert.Equal(6, y);
+
+
+            // Praktijkvoorbeeld van een out-parameter:
+            string value;
+            var dict = new Dictionary<int, string>();
+            dict.TryGetValue(23, out value);
+        }
+
+        private void UpdateWithOut(out int y)
+        {
+            y = 6;
+        }
+    }
+}
