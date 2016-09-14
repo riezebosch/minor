2720c0f71a5f93752b7e379c25aaa4677cf7919f
Manuel Riezebosch <mriezebosch@gmail.com>
2016-09-14 13:54:08 +0200

    Add method increases count

---
+++ src/TddDemo/ExtensionMethodsDemo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using System.Reflection;
+
+namespace TddDemo
+{
+    static class SomeClassExt
+    {
+        public static void Update(this SomeClass item, int value)
+        {
+            // Dit mag nu niet: item._i = value;
+            typeof(SomeClass)
+                .GetField("_i", BindingFlags.NonPublic | BindingFlags.Instance)
+                .SetValue(item, value);
+        }
+
+        public static string ConvertToString(this DayOfWeek day)
+        {
+            return "alle dagen zijn goed";
+        }
+    }
+    class SomeClass
+    {
+        private int _i;
+        public SomeClass()
+        {
+        }
+
+        public int I => _i;
+    }
+
+    public class ExtensionMethodsDemo
+    {
+
+
+
+
+        [Fact]
+        public void WatZijnExtensionMethodsEigenlijk()
+        {
+            var s = new SomeClass();
+
+            s.Update(3);
+            SomeClassExt.Update(s, 3);
+
+            Assert.Equal(3, s.I);
+
+            DayOfWeek.Friday.ConvertToString();
+        }
+    }
+}
---
+++ src/TddDemo/LinkedListTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TddDemo
+{
+    public class LinkedListTest
+    {
+        public int Count { get; private set; }
+
+        [Fact]
+        public void GivenEmptyListWhenAddThenCount1()
+        {
+            Add(3);
+            Assert.Equal<int>(1, Count);
+        }
+
+        private void Add(int item)
+        {
+            Count++;
+        }
+    }
+}
