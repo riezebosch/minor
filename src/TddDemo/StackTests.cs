4db0347db8709bbd59ed93a6b21d39b284f7222e
Manuel Riezebosch <mriezebosch@gmail.com>
2016-09-22 10:57:37 +0200

    performance probleem op de stack

--- src/TddDemo/StackTests.cs
+++ src/TddDemo/StackTests.cs
@@ -1,4 +1,7 @@
-﻿using Xunit;
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using Xunit;
 
 namespace TddDemo
 {
@@ -26,6 +29,44 @@ namespace TddDemo
             stack.Pop();
 
             Assert.True(stack.IsEmpty());
+        }
+
+        [Fact]
+        public void PerformanceVanEenStack()
+        {
+            var items = new Stack<int>();
+            var sw = Stopwatch.StartNew();
+
+            Enumerable
+                .Range(0, 100000)
+                .ToList()
+                .ForEach(i => items.Put(i));
+
+            while (!items.IsEmpty())
+            {
+                items.Pop();
+            }
+
+            Assert.True(sw.Elapsed < TimeSpan.FromSeconds(60));
+        }
+
+        [Fact]
+        public void PerformanceVanEenQueue()
+        {
+            var items = new Queue<int>();
+            var sw = Stopwatch.StartNew();
+
+            Enumerable
+                .Range(0, 100000)
+                .ToList()
+                .ForEach(i => items.Enqueue(i));
+
+            while (!items.IsEmpty())
+            {
+                items.Dequeue();
+            }
+
+            Assert.True(sw.Elapsed < TimeSpan.FromSeconds(60));
         }
     }
 }
