644ff20bcc51b5cb25ebe65256c42df31d664baf
Manuel Riezebosch <mriezebosch@gmail.com>
2016-09-22 10:15:06 +0200

    tolookup demo

---
+++ src/TddDemo/CollectionsDemo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TddDemo
+{
+    public class CollectionsDemo
+    {
+        [Fact]
+        public void ToLookUp()
+        {
+            int[] items = { 1, 2, 3, 4 };
+            var result = items.ToLookup(i => i % 2 == 0);
+            
+            Assert.Equal(new[] { 2, 4 }, result[true]);
+            Assert.Equal(new[] { 1, 3 }, result[false]);
+        }
+    }
+}
