ba3d485b8ae602d4766c3260bd84ee0220c0cd98
Manuel Riezebosch <mriezebosch@gmail.com>
2016-09-27 10:25:28 +0200

    custom equalitycomparer voor assert.equals van xunit

---
+++ src/TddDemo/LinqOefeningMetStudenten.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TddDemo
+{
+    public class LinqOefeningMetStudenten
+    {
+        [Fact]
+        public void HoeWerktAssertMetSingleObjectEquality()
+        {
+            var student = new Student { Naam = "Pieter", Nummer = 1 };
+            var result = new Student { Naam = "Pieter", Nummer = 1 };
+
+            Assert.Equal(student, result, new StudentComparer());
+        }
+
+        private class Student
+        {
+            public string Naam { get; set; }
+            public int Nummer { get; set; }
+        }
+
+        private class StudentComparer : IEqualityComparer<Student>
+        {
+            public bool Equals(Student x, Student y)
+            {
+                return x.Naam == y.Naam &&
+                    x.Nummer == y.Nummer;
+            }
+
+            public int GetHashCode(Student obj)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+
+
+}
