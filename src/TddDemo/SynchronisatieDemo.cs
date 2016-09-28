c8609f7602bbe05732cbfcbe3e77df2d117bacef
Manuel Riezebosch <mriezebosch@gmail.com>
2016-09-28 13:33:07 +0200

    event op andere thread met delay

---
+++ src/TddDemo/SynchronisatieDemo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TddDemo
+{
+    class SomeEventRaisingThing
+    {
+        public event Action Raise;
+
+
+        public void RaiseEventOnSeperateThread()
+        {
+            Task.Run(() => 
+            {
+                Task.Delay(TimeSpan.FromSeconds(2));
+                Raise?.Invoke();
+            });
+        }
+
+    }
+    public class SynchronisatieDemo
+    {
+        private bool isRaised;
+
+        [Fact]
+        public void HoeSynchroniseerIkIetsWatOpVerschillendeThreadsWordtUitgevoerd()
+        {
+            var thing = new SomeEventRaisingThing();
+            thing.Raise += React;
+            thing.RaiseEventOnSeperateThread();
+
+            Assert.True(isRaised);
+        }
+
+        private void React()
+        {
+            isRaised = true;
+        }
+    }
+}
