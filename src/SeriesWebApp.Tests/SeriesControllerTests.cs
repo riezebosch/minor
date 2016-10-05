087ab31b16c987d13d71cc91a1959b60a5e1d16c
Manuel Riezebosch <mriezebosch@gmail.com>
2016-10-05 15:43:53 +0200

    Lege controller tests

---
+++ src/SeriesWebApp.Tests/SeriesControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SeriesWebApp.Tests
+{
+    public class SeriesControllerTests
+    {
+        [Fact]
+        public void IndexShouldIncludeSeasonsInModel()
+        {
+
+        }
+    }
+}
